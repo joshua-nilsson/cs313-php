6d711a6aed3e806c2a4c2b3beefa4ec9490d21f3
DELL-XPS-8500\Joshua <nil10002@byui.edu>
2018-05-21 21:57:56 -0600

    updated file

--- web/team-activity/week5/team5.php
+++ web/team-activity/week5/team5.php
@@ -1,6 +1,6 @@
 
 <!DOCTYPE HTML>
-<html>  
+<html>
 <body>
 
 <form action="05TeachTeam.php" method="get">
@@ -8,9 +8,9 @@
 <?php
 try
 {
-  $user = 'userName';
-  $password = 'passswordSalt';
-  $db = new PDO('pgsql:host=..........;dbname=dbnameonServer', $user, $password);
+  $user = 'hhhrcjrfjyglyn';
+  $password = 'e5a22e741fe1d44d973e341c1291e949531799efbd5872116115ce25cd5ca9d8';
+  $db = new PDO('pgsql:host=ec2-50-19-224-165.compute-1.amazonaws.com;dbname=d2gtblt01rf5ff', $user, $password);
 }
 catch (PDOException $ex)
 {
@@ -22,9 +22,9 @@ $statement = $db->query('SELECT DISTINCT book FROM scriptures');
  echo'<tr>';
 while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 {
- 
+
   echo '<th><input type="checkbox" name="check[]" id="check" value="'. $row['book'] .'">'. $row['book'] .'</th><br>';
- 
+
 }
  echo '</tr>';
 
