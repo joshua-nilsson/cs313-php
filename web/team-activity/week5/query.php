78e0b115a6d49c0935d7a93cc6907112822853b2
DELL-XPS-8500\Joshua <nil10002@byui.edu>
2018-05-21 22:04:36 -0600

    updated file

--- web/team-activity/week5/query.php
+++ web/team-activity/week5/query.php
@@ -7,22 +7,22 @@ $listOfScriptues = $_GET["check"];
 <?php
 try
 {
-  $user = 'userName';
-  $password = 'passswordSalt';
-  $db = new PDO('pgsql:host=..........;dbname=dbnameonServer', $user, $password);
+  $user = 'hhhrcjrfjyglyn';
+  $password = 'e5a22e741fe1d44d973e341c1291e949531799efbd5872116115ce25cd5ca9d8';
+  $db = new PDO('pgsql:host=ec2-50-19-224-165.compute-1.amazonaws.com;dbname=d2gtblt01rf5ff', $user, $password);
 }
 catch (PDOException $ex)
 {
   echo 'Error!: ' . $ex->getMessage();
   die();
 }
-	 
+
 foreach($listOfScriptues as $scripture){
-	
-	$statement = $db->query("SELECT book, chapter, verse FROM scriptures where book = '".$scripture."';");
-	while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-	{
-	  echo 'Scriptures: ' . $row['book'] . " " .$row['chapter'] .':'. $row['verse'] . '<br/>';
-	}
+
+  $statement = $db->query("SELECT book, chapter, verse FROM scriptures where book = '".$scripture."';");
+  while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+  {
+    echo 'Scriptures: ' . $row['book'] . " " .$row['chapter'] .':'. $row['verse'] . '<br/>';
+  }
 }
 ?>
