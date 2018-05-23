60e41b061dcb37ad2bdec673c92fdb7477c8ab92
DELL-XPS-8500\Joshua <nil10002@byui.edu>
2018-05-22 18:18:51 -0600

    initial commit ta5

---
+++ web/team-activity/week5/content.php
@@ -0,0 +1,30 @@
+<?php
+// Start the session
+session_start();
+$scriptureId = $_GET["value_key"];
+
+try{
+  $dbUrl = getenv('DATABASE_URL');
+  $dbopts = parse_url($dbUrl);
+  $dbHost = $dbopts["host"];
+  $dbPort = $dbopts["port"];
+  $dbUser = $dbopts["user"];
+  $dbPassword = $dbopts["pass"];
+  if(!empty($dbopts["path"])){
+    $dbName = ltrim($dbopts["path"],'/');
+  }else{
+    $dbName = $dbase;
+  }
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+$statement = $db->query('SELECT book, chapter, verse, content FROM scriptures where scripture_id = '.$scriptureId.';');
+while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+{
+  echo 'Content: ' . $row['book'] . " " .$row['chapter'] .':'. $row['verse'] . ' ' . $row['content'] .'<br/>';
+}
+?>
--- web/team-activity/week5/team5.php
+++ web/team-activity/week5/team5.php
@@ -1,38 +1,47 @@
-
+<?php
+// Start the session
+session_start();
+?>
 <!DOCTYPE HTML>
 <html>
 <body>
 
-<form action="query.php" method="get">
+  <form action="scriptures.php" method="get">
 
-<?php
-try
-{
-  $user = 'hhhrcjrfjyglyn';
-  $password = 'e5a22e741fe1d44d973e341c1291e949531799efbd5872116115ce25cd5ca9d8';
-  $db = new PDO('pgsql:host=ec2-50-19-224-165.compute-1.amazonaws.com;dbname=d2gtblt01rf5ff', $user, $password);
-}
-catch (PDOException $ex)
-{
-  echo 'Error!: ' . $ex->getMessage();
-  die();
-}
+    <?php
+    try{
+      $dbUrl = getenv('DATABASE_URL');
+      $dbopts = parse_url($dbUrl);
+      $dbHost = $dbopts["host"];
+      $dbPort = $dbopts["port"];
+      $dbUser = $dbopts["user"];
+      $dbPassword = $dbopts["pass"];
+      if(!empty($dbopts["path"])){
+        $dbName = ltrim($dbopts["path"],'/');
+      }else{
+        $dbName = $dbase;
+      }
+      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    }
+    catch (PDOException $ex)
+    {
+      echo 'Error!: ' . $ex->getMessage();
+      die();
+    }
 
-$statement = $db->query('SELECT DISTINCT book FROM scriptures');
- echo'<tr>';
-while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-{
-
-  echo '<th><input type="checkbox" name="check[]" id="check" value="'. $row['book'] .'">'. $row['book'] .'</th><br>';
-
-}
- echo '</tr>';
+    $statement = $db->query('SELECT DISTINCT book FROM scriptures');
+    echo'<tr>';
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+    {
+      echo '<th><input type="checkbox" name="check[]" id="check" value="'. $row['book'] .'">'. $row['book'] .'</th><br>';
+    }
+    echo '</tr>';
 
 
-?>
+    ?>
 
-   <input type="submit" value="submit">
+    <input type="submit" value="submit">
 
-</form>
+  </form>
 </body>
 </html>
