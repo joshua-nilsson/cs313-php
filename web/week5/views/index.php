f99e868c02f035e238caca87fd65ea0da70c7b58
DELL-XPS-8500\Joshua <nil10002@byui.edu>
2018-05-28 18:02:01 -0600

    db test

--- web/week5/views/index.php
+++ web/week5/views/index.php
@@ -52,7 +52,7 @@ catch (PDOException $ex)
 
       while ($row = $statement->fetch(PDO::FETCH_ASSOC))
       {
-        echo 'NAME: ' $row['nametext'];
+        echo 'NAME: ' . $row['nametext'];
       }
 
 
