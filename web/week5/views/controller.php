720d9c1b75341df3d824bc5ad277c68e01c5e520
DELL-XPS-8500\Joshua <nil10002@byui.edu>
2018-05-28 19:52:51 -0600

    db test

--- web/week5/views/controller.php
+++ web/week5/views/controller.php
@@ -39,44 +39,44 @@ switch ($action) {
 //      $row = $statement->fetch(PDO::FETCH_ASSOC)
 //      echo '<p>NAME: ' . $row['nametext'] . '</p>';
 //   }
-    $prompt = "<div>";
+//    $prompt = "<div>";
+    $prompt = "<table class='table'>";
+    $prompt .= "<thead id='head-dark' class='thead-dark'>";
+    $prompt .= '<tr>';
+    $prompt .= "<th scope='col'>PROMPT</th>";
+    $prompt .= "<th scope='col'>NAME</th>";
+    $prompt .= "<th scope='col'>CONTROLS</th>";
+    $prompt .= '</tr>';
+    $prompt .= '</thead>';
+    $prompt .= '<tbody>';
     while ($row = $statement->fetch(PDO::FETCH_ASSOC))
     {
 
-        $prompt .= '<p>Name: ' . $row['nametext'] . '</p>';
-//      $prompt = "<table class='table'>";
-//      $prompt .= "<thead id='head-dark' class='thead-dark'>";
-//      $prompt .= '<tr>';
-//      $prompt .= "<th scope='col'>PROMPT</th>";
-//      $prompt .= "<th scope='col'>NAME</th>";
-//      $prompt .= "<th scope='col'>CONTROLS</th>";
-//      $prompt .= '</tr>';
-//      $prompt .= '</thead>';
-//      $prompt .= '<tbody>';
-//      $prompt .= '<tr>';
-//      $prompt .= '<td>';
-//      $prompt .= "<div class='input-group input-group-default mb-6'>";
-//      $prompt .= "<div class='input-group-prepend'>";
-//      $prompt .= "<div class='input-group-text' id='inputGroup-sizing-sm'>SuikkLakan</div>";
-//      $prompt .= '</div>';
-//      $prompt .= '</div>';
-//      $prompt .= '</td>';
-//      $prompt .= '<td>';
-//      $prompt .= "<div class='input-group-append'>";
-//      $prompt .= "<input type='text' class='form-control' aria-label='Small' aria-describedby='inputGroup-sizing-lg' value="$row[nametext]">";
-//      $prompt .= '</div>';
-//      $prompt .= '</td>';
-//      $prompt .= '<td>';
-//      $prompt .= "<div class='input-group-append'>";
-//      $prompt .= "<button class='btn btn-primary' type='button'>SUBMIT</button>";
-//      $prompt .= '</div>';
-//      $prompt .= '</td>';
-//      $prompt .= '</tr>';
-//      $prompt .= '</tbody>';
-//      $prompt .= '</table>';
+//        $prompt .= '<p>Name: ' . $row['nametext'] . '</p>';
+      $prompt .= '<tr>';
+      $prompt .= '<td>';
+      $prompt .= "<div class='input-group input-group-default mb-6'>";
+      $prompt .= "<div class='input-group-prepend'>";
+      $prompt .= "<div class='input-group-text' id='inputGroup-sizing-sm'>SuikkLakan</div>";
+      $prompt .= '</div>';
+      $prompt .= '</div>';
+      $prompt .= '</td>';
+      $prompt .= '<td>';
+      $prompt .= "<div class='input-group-append'>";
+      $prompt .= "<input type='text' class='form-control' aria-label='Small' aria-describedby='inputGroup-sizing-lg' value='$row[nametext]'>";
+      $prompt .= '</div>';
+      $prompt .= '</td>';
+      $prompt .= '<td>';
+      $prompt .= "<div class='input-group-append'>";
+      $prompt .= "<button class='btn btn-primary' type='button'>SUBMIT</button>";
+      $prompt .= '</div>';
+      $prompt .= '</td>';
+      $prompt .= '</tr>';
     }
+    $prompt .= '</tbody>';
+    $prompt .= '</table>';
 
-    $prompt .= "</div";
+//    $prompt .= "</div";
 
     include 'index.php';
     break;
