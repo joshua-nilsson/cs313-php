62d7557d3ad5675f064cdbc4cc43378ea2a3a716
DELL-XPS-8500\Joshua <nil10002@byui.edu>
2018-05-31 23:13:20 -0600

    initial commit

--- web/team-activity/week7/login7.php
+++ web/team-activity/week7/login7.php
@@ -1,4 +1,26 @@
-<?php dbConnect(); ?>
+<?php
+session_start();
+
+try{
+  $dbUrl = getenv('DATABASE_URL');
+  $dbopts = parse_url($dbUrl);
+  $dbHost = $dbopts["host"];
+  $dbPort = $dbopts["port"];
+  $dbUser = $dbopts["user"];
+  $dbPassword = $dbopts["pass"];
+  if(!empty($dbopts["path"])){
+    $dbName = ltrim($dbopts["path"],'/');
+  }else{
+    $dbName = $dbase;
+  }
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+?>
 <!DOCTYPE HTML>
 <html lang="en-US">
   <head>
