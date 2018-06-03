675c01f97deba2a0dc14c8ba68aa5e9c27fad74a
DELL-XPS-8500\Joshua <nil10002@byui.edu>
2018-06-03 11:11:08 -0600

    initial commit

--- web/week6/views/controller.php
+++ web/week6/views/controller.php
@@ -81,7 +81,7 @@ switch ($action) {
       $prompt .= '</td>';
       $prompt .= '<td>';
       $prompt .= "<div class='input-group-append'>";
-      $prompt .= "<button class='btn btn-primary' type='button' title='Click to Submit'>SUBMIT</i></button>";
+      $prompt .= "<a href='controller.php?action=insert' class='btn btn-primary' title='Click to Submit'>SUBMIT</i></a>";
       $prompt .= "<input type='hidden' name='action' value='insert'>";
       $prompt .= '</div>';
       $prompt .= '</td>';
