85d4b43b928669d4a49febca5a7db085c6b06a6d
DELL-XPS-8500\Joshua <nil10002@byui.edu>
2018-06-02 23:50:12 -0600

    initial commit

--- web/week6/modules/header.php
+++ web/week6/modules/header.php
@@ -6,22 +6,21 @@
           <li class="nav-item"><a href="index.php">Fantasy Name Generator</a></li>
         </div>
         <div class="col-sm-1">
+          <li class="nav-item"><a href="account.php">Account</a></li>
+        </div>
+        <div class="col-sm-1">
           <li class="nav-item">
-            <a href="account.php">
+            <a href="login.php">
               <?php
-              if (isset($_SESSION['loggedin'])){
-
-                $username = $_SESSION['clientData']['clientusername'];
-
-                echo $username . "'s Account";
+              if (isset($_SESSION['loggedin'])) {
+                echo "<a href='/acme/accounts/?action=logout' title='Click to Logout'>Logout</a>";
               }
-              else { echo "Account";}
+              else {
+                echo "<a href='login.php'>Login</a>";
+              }
               ?>
             </a>
           </li>
-        </div>
-        <div class="col-sm-1">
-          <li class="nav-item"><a href="login.php">Login</a></li>
         </div>
       </div>
     </div>
