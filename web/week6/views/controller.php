4152f73eb284d92f116d061b3821e7d81de0b3e4
DELL-XPS-8500\Joshua <nil10002@byui.edu>
2018-06-08 13:01:36 -0600

    update

--- web/week6/views/controller.php
+++ web/week6/views/controller.php
@@ -331,26 +331,26 @@ function getClient($clientusername){
   return $clientData;
 }
 function clientCollection($clientId) {
-//  session_start();
-//  try{
-//    $dbUrl = getenv('DATABASE_URL');
-//    $dbopts = parse_url($dbUrl);
-//    $dbHost = $dbopts["host"];
-//    $dbPort = $dbopts["port"];
-//    $dbUser = $dbopts["user"];
-//    $dbPassword = $dbopts["pass"];
-//    if(!empty($dbopts["path"])){
-//      $dbName = ltrim($dbopts["path"],'/');
-//    }else{
-//      $dbName = $dbase;
-//    }
-//    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-//  }
-//  catch (PDOException $ex)
-//  {
-//    echo 'Error!: ' . $ex->getMessage();
-//    die();
-//  }
+  session_start();
+  try{
+    $dbUrl = getenv('DATABASE_URL');
+    $dbopts = parse_url($dbUrl);
+    $dbHost = $dbopts["host"];
+    $dbPort = $dbopts["port"];
+    $dbUser = $dbopts["user"];
+    $dbPassword = $dbopts["pass"];
+    if(!empty($dbopts["path"])){
+      $dbName = ltrim($dbopts["path"],'/');
+    }else{
+      $dbName = $dbase;
+    }
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+  }
+  catch (PDOException $ex)
+  {
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+  }
 
   $id = $_SESSION['clientData']['clientid'];
   $collection = $db->query("SELECT collectiontext FROM collection WHERE clientid = '$id'");
