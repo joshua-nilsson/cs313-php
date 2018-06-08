dc246229ab7786fbb83943cac5c4f214119afda0
DELL-XPS-8500\Joshua <nil10002@byui.edu>
2018-06-08 10:28:24 -0600

    update

--- web/week6/views/login.php
+++ web/week6/views/login.php
@@ -51,7 +51,7 @@ catch (PDOException $ex)
                 <input type="password" name="clientpassword" class="form-control" placeholder="FunkyChicken92!">
                 <input type="submit" value="LOGIN" id="log" class="btn btn-primary">
                 <input type="hidden" name="action" value="login">
-                <a href="register.php" class="btn btn-secondary">REGISTER</a>
+                <p><a href="register.php" class="btn btn-secondary">REGISTER</a></p>
               </div>
             </form>
           </div>
