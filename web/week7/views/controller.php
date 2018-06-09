cfece7451fdb447ce04257279fd3e2c8c7567594
DELL-XPS-8500\Joshua <nil10002@byui.edu>
2018-06-09 13:08:57 -0600

    testing update

--- web/week7/views/controller.php
+++ web/week7/views/controller.php
@@ -177,10 +177,12 @@ switch ($action) {
     $collection .= "<table class='table'>";
     $collection .= "<thead id='head-dark' class='thead-dark'>";
     $collection .= '<tr>';
+    $collection .= "<div class='row'>";
     $collection .= "<th scope='col'>PROMPT</th>";
     $collection .= "<th scope='col' class='col-sm-1'>NAME</th>";
     $collection .= "<th scope='col' class='col-sm-1'>CONTROLS</th>";
     $collection .= "<th scope='col' class='col-sm-1'></th>";
+    $collection .= '</div>';
     $collection .= '</tr>';
     $collection .= '</thead>';
     $collection .= '<tbody>';
