cc5139660eb774431cbacb0ffcb4256c2e63b3b1
DELL-XPS-8500\Joshua <nil10002@byui.edu>
2018-06-08 22:57:59 -0600

    testing update

--- web/week7/views/index.php
+++ web/week7/views/index.php
@@ -45,8 +45,6 @@ if (!$_SESSION['loggedin']) {
 
       <?php if (isset($prompt)){echo $prompt;}?>
       <?php if (isset($prompt)){echo $collection;}?>
-
-<!--      <?php include '../modules/modal.php';?>-->
     </main>
 
     <footer>
