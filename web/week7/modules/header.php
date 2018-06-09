098e054cedc9bd290e11c3858655d506045ee8bd
DELL-XPS-8500\Joshua <nil10002@byui.edu>
2018-06-09 14:53:12 -0600

    testing update

--- web/week7/modules/header.php
+++ web/week7/modules/header.php
@@ -1,22 +1,20 @@
 <nav>
-  <div class="container">
-    <ul class="nav">
+  <ul class="nav">
+    <div class="container">
       <div class="row">
         <div class="col-sm-10">
           <li class="nav-item"><a href="index.php">Fantasy Name Generator</a></li>
         </div>
         <div class="col-sm-1">
           <li class="nav-item">
-            <a href="account.php">
-              <?php
-              if (isset($_SESSION['loggedin'])) {
-                echo "<a href='account.php' title='View Account'>Account</a>";
-              }
-              else {
-                echo "<a href='register.php' title='Click to Register'>Register</a>";
-              }
-              ?>
-            </a>
+            <?php
+            if (isset($_SESSION['loggedin'])) {
+              echo "<a href='account.php' title='View Account'>Account</a>";
+            }
+            else {
+              echo "<a href='register.php' title='Click to Register'>Register</a>";
+            }
+            ?>
           </li>
         </div>
         <div class="col-sm-1">
@@ -32,6 +30,6 @@
           </li>
         </div>
       </div>
-    </ul>
-  </div>
+    </div>
+  </ul>
 </nav>
