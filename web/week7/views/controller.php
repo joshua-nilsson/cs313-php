6ea2a060b7d40ace9fb75973053c7a506db73eb4
DELL-XPS-8500\Joshua <nil10002@byui.edu>
2018-06-09 12:14:36 -0600

    testing update

--- web/week7/views/controller.php
+++ web/week7/views/controller.php
@@ -200,13 +200,13 @@ switch ($action) {
       $collection .= "<div class='input-group-append'>";
       $collection .= "<input type='text' name='collectiontext' class='form-control' aria-label='Small' aria-describedby='inputGroup-sizing-lg'>";
       $collection .= '</div>';
+      $collection .= '</td>';
+      $collection .= '<td>';
       $collection .= "<div class='input-group-append'>";
       $collection .= "<button type='submit' class='btn btn-warning' title='Click to Update'><i class='fas fa-sync-alt fa-fw'></i></button>";
       $collection .= "<input type='hidden' name='collectionid' value='$row[collectionid]'>";
       $collection .= "<input type='hidden' name='action' value='update'>";
       $collection .= '</div>';
-      $collection .= '</td>';
-      $collection .= '<td>';
       $collection .= "<div class='input-group-append'>";
       $collection .= "<button type='submit' class='btn btn-danger' title='Click to Delete'><i class='fas fa-trash-alt fa-fw'></i></button>"; // input not button - but how do you submit then with no submit button?
       $collection .= "<input type='hidden' name='collectionid' value='$row[collectionid]'>";
