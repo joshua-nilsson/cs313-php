2cdbfa9c3a7808d935fbe36477f3984396c2ac03
DELL-XPS-8500\Joshua <nil10002@byui.edu>
2018-06-09 12:28:02 -0600

    testing update

--- web/week7/modules/modal.php
+++ web/week7/modules/modal.php
@@ -8,7 +8,7 @@
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
-      <form>
+      <form action="controller.php" method="post">
       <div class="modal-body">
         <p>Are you sure you want to delete this name?</p>
       </div>
@@ -16,8 +16,8 @@
         <button type="button" class="btn btn-secondary" data-dismiss="modal" title="Click to Cancel"><i class="fas fa-ban fa-2x"></i></button>
         <button type="submit" class="btn btn-danger" title="Click to Delete"><i class='fas fa-trash-alt fa-fw fa-2x'></i></button>
       </div>
-        <input type='hidden' name='collectionid' value="<?php echo $row['collectionid'];?>">
-        <input type='hidden' name='action' value='delete'>
+        <input type="hidden" name="collectionid" value="<?php echo $row['collectionid'];?>">
+        <input type="hidden" name="action" value="delete">
       </form>
     </div>
   </div>
