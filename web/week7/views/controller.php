c00164ede953a0afb22b26de8ed26a25d9075037
DELL-XPS-8500\Joshua <nil10002@byui.edu>
2018-06-09 12:05:49 -0600

    testing update

--- web/week7/views/controller.php
+++ web/week7/views/controller.php
@@ -204,6 +204,7 @@ switch ($action) {
       $collection .= "<button type='submit' class='btn btn-warning' title='Click to Update'><i class='fas fa-sync-alt fa-fw'></i></button>";
       $collection .= "<input type='hidden' name='collectionid' value='$row[collectionid]'>";
       $collection .= "<input type='hidden' name='action' value='update'>";
+      $collection .= '</div>';
       $collection .= '</form>';
       $collection .= '</td>';
       $collection .= '<td>';
